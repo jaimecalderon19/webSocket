5dcfe6e43ba0e24189e7a1078ecdd89fcd4b177b
Jaime Andres <jaimebc.yt@gmail.com>
2024-02-19 11:48:35 -0500

    update websocket

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
-import { generateUniqueID, handleRoom, handleUsername } from "./utils";
+import { ServerWebSocket } from "bun";
+import { generateUniqueID } from "./utils";
 
 const user = {
   room: "notifications",
@@ -7,8 +8,14 @@ const user = {
 
 export type User = typeof user;
 
+const arrayClients:Array<ServerWebSocket<User>> = [];
+
 const server = Bun.serve<User>({
   fetch(req, server) {
+    const url = new URL(req.url);
+    const room = url.searchParams.get("room");
+    user.room = room ? room : 'notifications';
+
     if (server.upgrade(req, { data: { ...user } })) return;
     return new Response(null, {
       status: 301,
@@ -17,35 +24,47 @@ const server = Bun.serve<User>({
       },
     });
   },
+
   websocket: {
     open(ws) {
-	  ws.subscribe(ws.data.room)
-	  ws.data.userid = generateUniqueID();
-      ws.send(`username: ${ws.data.userid} room: ${ws.data.room}`);
+	    ws.subscribe(ws.data.room)
+	    ws.data.userid = generateUniqueID();
+      ws.send(`userId: ${ws.data.userid} room: ${ws.data.room}`);
+      arrayClients.push(ws);
     },
+
     message(ws, message) {
       if (typeof message !== "string") return;
 
       try {
         const parsedMessage = JSON.parse(message);
 
-        if (parsedMessage.to && parsedMessage.to === ws.data.userid) {
-          // Verificar si el mensaje tiene un destinatario específico
-          const responseMessage = {
-            type: "message",
-            content: parsedMessage.content,
-          };
-          ws.send(JSON.stringify(responseMessage));
-        } else {
-          // Mensaje regular para la sala
-		  console.log('handleUsername');
+        arrayClients.forEach(client => {
+          if (parsedMessage.to && parsedMessage.to === client.data.userid) {
+            // Verificar si el mensaje tiene un destinatario específico
+            console.log("enconro un user");
+            const responseMessage = {
+              type: "message",
+              content: parsedMessage.content,
+            };
+            client.send(JSON.stringify(responseMessage));
+          }
+        });
+
+        // if (parsedMessage.to && parsedMessage.to === ws.data.userid) {
+        //   // Verificar si el mensaje tiene un destinatario específico
+        //   const responseMessage = {
+        //     type: "message",
+        //     content: parsedMessage.content,
+        //   };
+        //   ws.send(JSON.stringify(responseMessage));
+        // } else {
+
 		  
-          if (!ws.data.userid) return handleUsername(ws, message);
-		  console.log('handleRoom');
-          if (!ws.data.room) return handleRoom(ws, message);
-		  console.log('publish');
-          ws.publish(ws.data.room, `${ws.data.userid}: ${message}`);
-        }
+        //   if (!ws.data.userid) return handleUsername(ws, message);
+        //   if (!ws.data.room) return handleRoom(ws, message);
+        //   ws.publish(ws.data.room, `${ws.data.userid}: ${message}`);
+        // }
       } catch (error) {
         console.error("Error al analizar el mensaje:", error);
       }
@@ -63,4 +82,3 @@ process.on("SIGINT", () => {
   server.stop();
 });
 
-// Resto del código de utils...
